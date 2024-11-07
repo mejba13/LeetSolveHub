f93ddbc4863fbd002cbcd9e68c998e53771d2045
mejba13 <mejba.13@gmail.com>
2024-11-07 10:10:41 +0600

    add: solution for maximum number of vowels in a substring of given length using sliding window technique

--- Sliding Window/maximum_vowels_in_substring.php
+++ Sliding Window/maximum_vowels_in_substring.php
@@ -16,17 +16,17 @@ class Solution {
 
         $max_vowel = $current_vowel;
 
-      for ($i = $k ; $i < strlen($s); $i++) {
-          if (in_array($s[$i - $k], $Vowels)) {
-              $current_vowel--;
-          }
-          if(in_array($s[$i], $Vowels)){
-              $current_vowel++;
-          }
-          $max_vowel = max ($max_vowel, $current_vowel);
-      }
+        for ($i = $k ; $i < strlen($s); $i++) {
+            if (in_array($s[$i - $k], $Vowels)) {
+                $current_vowel--;
+            }
+            if(in_array($s[$i], $Vowels)){
+                $current_vowel++;
+            }
+            $max_vowel = max ($max_vowel, $current_vowel);
+        }
 
-      return $max_vowel;
+        return $max_vowel;
 
     }
 
