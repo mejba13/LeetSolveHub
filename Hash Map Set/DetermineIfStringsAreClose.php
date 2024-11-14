345f38a68cffd367c8247da2343362bfb2c1b99a
mejba13 <mejba.13@gmail.com>
2024-11-14 09:45:16 +0600

    Fix character frequency comparison in closeStrings function for determining if two strings are close

--- Hash Map Set/DetermineIfStringsAreClose.php
+++ Hash Map Set/DetermineIfStringsAreClose.php
@@ -10,14 +10,14 @@ class Solution {
         $count1 = array_count_values(str_split($word1));
         $count2 = array_count_values(str_split($word2));
 
-       if(count(array_diff_key($count1, $count2)) !== 0 || count(array_diff_key($count2, $count1)) !== 0) {
-           return false;
-       }
+        if(count(array_diff_key($count1, $count2)) !== 0 || count(array_diff_key($count2, $count1)) !== 0) {
+            return false;
+        }
 
         $value1 = array_values($count1);
         $value2 = array_values($count2);
 
-       print_r( sort($value1));
+        print_r( sort($value1));
         print_r( sort($value2));
 
         return $value1 == $value2;
