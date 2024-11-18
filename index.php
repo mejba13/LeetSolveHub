26655e8b3ab93a63c832e3b86356e0fec0b12f1c
mejba13 <mejba.13@gmail.com>
2024-11-18 07:58:28 +0600

    add solution for asteroid collision problem using stack

--- index.php
+++ index.php
@@ -1,21 +1,38 @@
 <?php
 
 class Solution {
-    function removeStars($s)
-    {
-       $stack = [];
 
-       for ($i = 0; $i < strlen($s); $i++) {
-           if($s[$i] == '*'){
-               array_pop($stack);
-           }else {
-               $stack[] = $s[$i];
-           }
-       }
-       return implode($stack);
+    function asteroidCollisions($asteroids){
+
+        $stack = [];
+
+        foreach ($asteroids as $asteroid) {
+            while (!empty($stack) && $asteroid < 0 && end($stack) > 0) {
+                if(abs($asteroid) > end($stack)){
+                    array_pop($stack);
+                }elseif (abs($asteroid) === end($stack)) {
+                    array_pop($stack);
+                    $asteroid = 0;
+                }else {
+                    $asteroid = 0;
+                }
+            }
+
+            if ($asteroid !== 0) {
+                $stack[] = $asteroid;
+            }
+        }
+
+        return $stack;
     }
 }
+
 $solution = new Solution();
-echo $solution->removeStars('leet**cod*e');
+$result = $solution->asteroidCollisions([5,10,-5]);
+print_r($result);
 echo "<br>";
-echo $solution->removeStars('erase*****');
+$result1 = $solution->asteroidCollisions([8,-8]);
+print_r($result1);
+echo "<br>";
+$result2 = $solution->asteroidCollisions([10,2,-5]);
+print_r($result2);
