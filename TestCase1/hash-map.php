6c7242ba005e571b3b73a8aa14a82ac515d71d29
mejba13 <mejba.13@gmail.com>
2024-11-23 08:56:49 +0600

    practice hasp map algorithom

--- TestCase1/hash-map.php
+++ TestCase1/hash-map.php
@@ -7,12 +7,12 @@ class solution {
         $map = [];
 
         for($i = 0; $i < count($nums); $i++){
-           $complement = $target - $nums[$i];
+            $complement = $target - $nums[$i];
 
-           if(isset($map[$complement])){
-               return [$map[$complement], $i];
-           }
-           $map[$nums[$i]] = $i;
+            if(isset($map[$complement])){
+                return [$map[$complement], $i];
+            }
+            $map[$nums[$i]] = $i;
         }
 
         return null;
